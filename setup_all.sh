b6c7d6b0aa033511cb6cc96c41c0659e8ee78368
Niklas Smedemark-Margulies <niklas.sm+github@gmail.com>
2020-09-10 18:19:33 -0400

    zsh

--- setup_all.sh
+++ setup_all.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
+# zsh
+sudo apt install zsh
+chsh -s $(which zsh) $(whoami)
+# Install oh-my-zsh (trusting their script)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Vim
 bash vim_setup.sh
---
+++ zsh_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cp .zshrc ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
