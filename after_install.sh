d8c72a6b164155eca10f9549a1a49bc04cb92ab5
Niklas <niklas.sm+github@gmail.com>
2020-11-10 23:23:14 -0500

    quietly()

--- after_install.sh
+++ after_install.sh
@@ -4,30 +4,37 @@ set -euo pipefail
 [[ $# -eq 1 ]] || { echo "missing arg: <home dir>" >&2; exit 1; }
 HOME=$1
 
-echo "apt update and install packages..."
-apt update -qq
-xargs -a apt_packages.txt apt install -qq
+quietly() {
+  eval $@
+} > /dev/null 2>&1
+
+echo "install apt packages..."
+quietly apt update -y
+quietly xargs -a apt_packages.txt apt install -y
 
 # global python packages
-python3 -m pip install virtualenv tldr
-python3 -m virtualenv ~/.venv
+quietly python3 -m pip install virtualenv tldr
+quietly python3 -m virtualenv ~/.venv
 
 # make ssh key if needed
 mkdir -p ~/.ssh
-[ -f ~/.ssh/id_rsa ] || ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+[ -f ~/.ssh/id_rsa ] || { 
+  echo "make ssh key..."
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "";
+}
 chmod -R 700 ~/.ssh/
 
 # python packages for vim ALE
 echo "install python packages..."
-source ~/.venv/bin/activate
-pip install --quiet -r ~/.requirements.txt
+quietly source ~/.venv/bin/activate
+quietly pip install -r ~/.requirements.txt
 
 # jupyter nbextensions
 echo "install jupyter extensions..."
-jupyter contrib nbextension install --user --log-level='ERROR'
-jupyter nbextensions_configurator enable --user --log-level='ERROR'
+quietly jupyter contrib nbextension install --user
+quietly jupyter nbextensions_configurator enable --user
 deactivate
 
-~/.fzf/install --all
+~/.fzf/install --all > /dev/null 2>&1
 
 chsh -s $(which zsh)
