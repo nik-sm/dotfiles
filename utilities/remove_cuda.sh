f4265a48839cf78dd0860aaa9c640e8f85d0c599
Niklas Smedemark-Margulies <niklas.sm+github@gmail.com>
2020-11-13 15:41:03 -0500

    update cuda scripts

--- utilities/remove_cuda.sh
+++ utilities/remove_cuda.sh
@@ -1,3 +1,6 @@
+# In case of "broken packages" preventing removal:
+# see https://askubuntu.com/questions/525088/how-to-delete-broken-packages-in-ubuntu
+# sudo dpkg --remove --force-remove-reinstreq package_name 
 sudo apt-get --purge -y remove 'cuda*'
 sudo apt-get --purge -y remove 'nvidia*'
 sudo reboot
