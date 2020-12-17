54803aa2ef6bbdf054462ccd45ef87f3c7ea13f9
Niklas <niklas.sm+github@gmail.com>
2020-12-17 00:01:46 -0500

    reorder and comments

--- after_install.sh
+++ after_install.sh
@@ -10,20 +10,10 @@ echo "install apt packages..."
 quietly apt update -y
 quietly xargs -a apt_packages.txt apt install -y
 
-# global python packages
-quietly python3 -m pip install virtualenv tldr
-quietly python3 -m virtualenv ~/.venv
-
-# make ssh key if needed
-mkdir -p ~/.ssh
-[ -f ~/.ssh/id_rsa ] || { 
-  echo "make ssh key..."
-  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "";
-}
-chmod -R 700 ~/.ssh/
-
 # python packages for vim ALE
 echo "install python packages..."
+quietly python3 -m pip install virtualenv tldr
+quietly python3 -m virtualenv ~/.venv
 quietly source ~/.venv/bin/activate
 quietly pip install -r ~/.requirements.txt
 
@@ -33,6 +23,16 @@ quietly jupyter contrib nbextension install --user
 quietly jupyter nbextensions_configurator enable --user
 deactivate
 
+# make ssh key if needed
+mkdir -p ~/.ssh
+[ -f ~/.ssh/id_rsa ] && echo "ssh key exists..." || { 
+  echo "make ssh key..."
+  ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "";
+}
+chmod -R 700 ~/.ssh/
+
+echo "install fzf..."
 ~/.fzf/install --all > /dev/null 2>&1
 
+echo "set zsh as default shell..."
 chsh -s $(which zsh)
